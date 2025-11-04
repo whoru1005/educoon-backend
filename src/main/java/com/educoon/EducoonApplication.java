ab14f7eb9f46a61398d94a09acf3a9a0f29ff124
whoru1005 <wjdtpwls1005@gmail.com>
2025-11-04 15:57:00 +0900

    inital commit

---
+++ src/main/java/com/educoon/EducoonApplication.java
@@ -0,0 +1,13 @@
+package com.educoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/educoon/EducoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
