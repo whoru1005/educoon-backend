baa5ddc658899ab8efdf04d3ab232b28e1e5469c
whoru1005 <wjdtpwls1005@gmail.com>
2025-11-15 15:33:19 +0900

    [FIX] 그룹 업데이트 시 설명 null 여부 수정

--- src/main/java/com/educoon/domain/studyRoom/StudyRoomUpdateRequest.java
+++ src/main/java/com/educoon/domain/studyRoom/StudyRoomUpdateRequest.java
@@ -18,8 +18,7 @@ public class StudyRoomUpdateRequest {
 
     @NotNull(message = "모집 인원은 필수입니다")
     private Integer maxCapacity;
-
-    @NotEmpty(message = "그룹 설명은 필수입니다")
+    
     private String description;
 
     @NotNull(message = "태그 목록은 필수입니다")
